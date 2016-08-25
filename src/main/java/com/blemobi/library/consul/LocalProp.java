30516c1e8344fed1501ec4b395b7081fddfa023f
李子才 <davis-sz@users.noreply.github.com>
2016-08-25 21:37:44 +0800

    修改公共库的consul

--- src/main/java/com/blemobi/library/consul/LocalProp.java
+++ src/main/java/com/blemobi/library/consul/LocalProp.java
@@ -12,17 +12,17 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class LocalProp {
 	private static HashMap<String,String> propInfo = new HashMap<String,String>();
-	private static HashMap<String,SocketInfo> services = new HashMap<String,SocketInfo>();
+	private static HashMap<String,SocketInfo[]> services = new HashMap<String,SocketInfo[]>();
 	
 	public static void invokeEnv(ConsulChangeListener adapter) {
-		for(Entry<String, SocketInfo> serv:services.entrySet()){
-			adapter.onServiceChange(serv.getKey(), new SocketInfo[]{serv.getValue()});
+		for(Entry<String, SocketInfo[]> serv:services.entrySet()){
+			adapter.onServiceChange(serv.getKey(), serv.getValue());
 		}
 		adapter.onEnvChange(propInfo);
 	}
 
 	public static void setLocalEnv(String filePath) throws IOException {
-
+		
 		String path = System.getProperty("user.dir")+File.separator+filePath;
 		InputStream in =new FileInputStream(path);
 
@@ -38,31 +38,28 @@ public class LocalProp {
 		for(Entry<Object, Object> p:fileProp.entrySet()){
 			String key = (String)(p.getKey());
 			String value = (String)p.getValue();
-			
-			log.info(key+" = ["+value+"]");
-
-			if(key.startsWith(prefix) && (key.endsWith(postfix_addr)) && (value.length() > (prefix.length()+postfix_addr.length()))){
+			if(key.startsWith(prefix) && (key.endsWith(postfix_addr)) && (key.length() > (prefix.length()+postfix_addr.length()))){
 				//如果是服务IP地址的信息
 				String serviceName = key.substring(0,(key.length()-postfix_addr.length())).substring(prefix.length());
-				SocketInfo si = getService(services,serviceName);
-				si.setIpAddr(value);
-			}else if(key.startsWith(prefix) && (key.endsWith(postfix_port)) && (value.length() > (prefix.length()+postfix_port.length()))){
+				SocketInfo[] si = services.get(serviceName);
+				if(si==null){
+					si = new SocketInfo[]{ new SocketInfo("nullAddr",-1)};
+					services.put(serviceName, si);
+				}
+				si[0].setIpAddr(value);
+			}else if(key.startsWith(prefix) && (key.endsWith(postfix_port)) && (key.length() > (prefix.length()+postfix_port.length()))){
 				//如果是服务端口的信息
 				String serviceName = key.substring(0,(key.length()-postfix_port.length())).substring(prefix.length());
-				SocketInfo si = getService(services,serviceName);
-				si.setPort(Integer.parseInt(value));
+				SocketInfo[] si = services.get(serviceName);
+				if(si==null){
+					si = new SocketInfo[]{ new SocketInfo("nullAddr",-1)};
+					services.put(serviceName, si);
+				}
+				si[0].setPort(Integer.parseInt(value));
 			}else{
 				propInfo.put(key, value);
 			}
 		}
 	}
 
-	private static SocketInfo getService(HashMap<String, SocketInfo> servs, String serviceName) {
-		SocketInfo rtn = servs.get(serviceName);
-		if(rtn==null){
-			rtn = new SocketInfo("nullAddr",-1);
-			servs.put(serviceName, rtn);
-		}
-		return rtn;
-	}
 }
