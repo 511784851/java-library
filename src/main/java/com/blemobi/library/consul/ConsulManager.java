eea9aa1d181874a49a713545dc4bbab2703e0835
李子才 <davis-sz@users.noreply.github.com>
2016-08-25 18:00:25 +0800

    引用公共库

--- src/main/java/com/blemobi/library/consul/ConsulManager.java
+++ src/main/java/com/blemobi/library/consul/ConsulManager.java
@@ -169,8 +169,11 @@ public class ConsulManager {
 	 * @param adapter 需要登记的适配器。
 	 */
 	public static void addConsulChangeListener(ConsulChangeListener adapter){
+		log.info("in addConsulChangeListener---------------");
 		if(!Strings.isNullOrEmpty(System.getProperty("EnvMode"))){
 			monitor.addConsulChangeListener(adapter);
+			log.info("------------------in addConsulChangeListener OK!");
+			
 		}else{
 			LocalProp.invokeEnv(adapter);
 		}
