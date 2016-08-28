3c2e365e319b8980aa44c0fe1788e662986308bf
zhaoyongandy <andy.zhao@blemobi.com>
2016-08-28 12:11:17 +0800

    修改

--- src/main/java/com/blemobi/library/redis/RedisManager.java
+++ src/main/java/com/blemobi/library/redis/RedisManager.java
@@ -1,20 +1,23 @@
 package com.blemobi.library.redis;
 
+import java.util.List;
+
 import com.blemobi.library.consul.BaseService;
 import com.google.common.base.Strings;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Transaction;
 
 public class RedisManager {
 	/*
 	 * 获得Redis连接
 	 */
 	public static Jedis getRedis() {
-		String auth = BaseService.getProperty("redis_user_auth");
-		RedisPoolSingleton redisPool = RedisPoolSingleton.getInstance();
-		JedisPool jedisPool = redisPool.getJedisPool();
+		JedisPool jedisPool = getJedisPool();
 		Jedis jedis = jedisPool.getResource();
+
+		String auth = BaseService.getProperty("redis_user_auth");
 		if (!Strings.isNullOrEmpty(auth)) {
 			jedis.auth(auth);
 		}
@@ -27,8 +30,22 @@ public class RedisManager {
 	 */
 	@SuppressWarnings("deprecation")
 	public static void returnResource(Jedis jedis) {
+		JedisPool jedisPool = getJedisPool();
+		jedisPool.returnResource(jedis);
+	}
+
+	/*
+	 * 释放Redis连接
+	 */
+	public static List<Object> returnResource(Jedis jedis, Transaction tx) {
+		List<Object> list = tx.exec();
+		returnResource(jedis);
+		return list;
+	}
+
+	private static JedisPool getJedisPool() {
 		RedisPoolSingleton redisPool = RedisPoolSingleton.getInstance();
 		JedisPool jedisPool = redisPool.getJedisPool();
-		jedisPool.returnResource(jedis);
+		return jedisPool;
 	}
 }
