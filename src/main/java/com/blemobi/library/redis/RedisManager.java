c98dadebe200854368e45c165bae2a55fc83b1f5
zhaoyongandy <andy.zhao@blemobi.com>
2016-08-30 11:06:10 +0800

    修改

--- src/main/java/com/blemobi/library/redis/RedisManager.java
+++ src/main/java/com/blemobi/library/redis/RedisManager.java
@@ -1,13 +1,10 @@
 package com.blemobi.library.redis;
 
-import java.util.List;
-
 import com.blemobi.library.consul.BaseService;
 import com.google.common.base.Strings;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.Transaction;
 
 public class RedisManager {
 	/*
@@ -29,18 +26,11 @@ public class RedisManager {
 	 * 释放Redis连接
 	 */
 	@SuppressWarnings("deprecation")
-	public static void returnResource(Jedis jedis) {
+	public static void returnResource(Jedis... jediss) {
 		JedisPool jedisPool = getJedisPool();
-		jedisPool.returnResource(jedis);
-	}
-
-	/*
-	 * 释放Redis连接
-	 */
-	public static List<Object> returnResource(Jedis jedis, Transaction tx) {
-		List<Object> list = tx.exec();
-		returnResource(jedis);
-		return list;
+		for (Jedis jedis : jediss) {
+			jedisPool.returnResource(jedis);
+		}
 	}
 
 	private static JedisPool getJedisPool() {
