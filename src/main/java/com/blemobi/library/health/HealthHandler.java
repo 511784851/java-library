c0e79a6b6a38a9eca303f913b0d7b4abf9b3ee82
zhaoyongandy <andy.zhao@blemobi.com>
2017-01-20 13:46:39 +0800

    恢复health代码

--- src/main/java/com/blemobi/library/health/HealthHandler.java
+++ src/main/java/com/blemobi/library/health/HealthHandler.java
@@ -1,36 +1,40 @@
 package com.blemobi.library.health;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.blemobi.sep.probuf.ResultProtos.PHealthItem;
-import com.blemobi.sep.probuf.ResultProtos.PServiceHealth;
-
 /**
- * 健康服务
- * 
- * @author zhaoyong
- *
+ * @author 李子才<davis.lee@blemobi.com>
+ * 这是聊天系统的健康服务发现的实现类，它是实现了HttpHandler接口。
  */
-public class HealthHandler extends HandlerAbstract {
 
-	/**
-	 * 构造方法
-	 * 
-	 * @param serviceName
-	 */
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.pakulov.jersey.protobuf.internal.MediaTypeExt;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class HealthHandler implements HttpHandler {
+
+	private String serviceName = "";
+
 	public HealthHandler(String serviceName) {
-		super(serviceName);
+		this.serviceName = serviceName;
 	}
 
-	/**
-	 * 返回数据
-	 */
-	@Override
-	protected byte[] getRespData() {
-		List<PHealthItem> list = new ArrayList<PHealthItem>();
-		PServiceHealth serviceHealth = PServiceHealth.newBuilder().setServiceName(serviceName).setStatus(1)
-				.addAllHealInfoList(list).build();
-		return serviceHealth.toByteArray();
+	public void handle(HttpExchange t) throws IOException {
+		String resp = readRespData();
+		byte[] pMessage = resp.getBytes();
+		t.sendResponseHeaders(200, pMessage.length);
+		Headers h = t.getResponseHeaders();
+		// 根据系统管理员要求，必须返回json格式的内容。因此在http的head里，要声明内容类型。
+		h.set("Content-Type", MediaTypeExt.APPLICATION_JSON);
+		OutputStream os = t.getResponseBody();
+		os.write(pMessage);
+		os.close();
+	}
+
+	// 根据系统管理员的要求，返回固定的json格式内容。
+	private String readRespData() {
+		return "{\"serviceName\" : \"" + this.serviceName + "\",\"isHealthy\" : true } ";
 	}
 }
--- src/main/java/com/blemobi/library/health/HealthManager.java
+++ src/main/java/com/blemobi/library/health/HealthManager.java
@@ -6,35 +6,25 @@ import com.sun.net.httpserver.HttpServer;
 
 import lombok.extern.log4j.Log4j;
 
-/**
- * 发布服务健康状态
- * 
- * @author zhaoyong
- *
- */
-@SuppressWarnings("restriction")
 @Log4j
 public class HealthManager {
 	/**
 	 * 启动对外的健康服务监听。
 	 * 
 	 * @param healthPort
-	 *            服务端口
-	 * @param serviceName
-	 *            服务名称
+	 *            健康服务的端口。
 	 */
 	public static void startService(int healthPort, String serviceName) {
 		try {
 			HttpServer server = HttpServer.create(new InetSocketAddress(healthPort), 0);
 			server.createContext("/", new HealthHandler(serviceName));
-			server.createContext("/status", new StatusHandler(serviceName));
 			server.setExecutor(null); // creates a default executor
 			server.start();
 			log.info("Health Report Server Running Port:" + healthPort);
 		} catch (Exception e) {
-			log.error("Health Report Server catch an exception, Port[" + healthPort + "] is used!");
-			log.error("System exit!");
-			log.error("Good bye!");
+			log.info("Health Report Server catch an exception, Port[" + healthPort + "] is used!");
+			log.info("System exit!");
+			log.info("Good bye!");
 
 			System.exit(0);
 		}
