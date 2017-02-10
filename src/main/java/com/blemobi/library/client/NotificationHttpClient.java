f4c337d2851498bed0ddb8874953d3b473a5303a
zhaoyongandy <andy.zhao@blemobi.com>
2017-02-10 15:05:21 +0800

    同步通知服务的调用

--- src/main/java/com/blemobi/library/client/NotificationHttpClient.java
+++ src/main/java/com/blemobi/library/client/NotificationHttpClient.java
@@ -3,8 +3,8 @@ package com.blemobi.library.client;
 import java.io.IOException;
 
 import com.blemobi.library.jetty.JettyServer;
-import com.blemobi.sep.probuf.NotificationApiProtos.PPushMsg;
 import com.blemobi.sep.probuf.ResultProtos.PMessage;
+import com.blemobi.sep.probuf.ResultProtos.PStringList;
 
 /**
  * 通知系统调用类
@@ -20,14 +20,22 @@ public class NotificationHttpClient extends BaseHttpClient {
 	/**
 	 * 通知消息
 	 * 
-	 * @param pushMsg
+	 * @param stringList
+	 *            消息接受者
+	 * @param uuid
+	 *            消息发送者
+	 * @param type
+	 *            消息id
+	 * @param type
+	 *            自定义参数值
 	 * @return
 	 * @throws IOException
 	 */
-	public PMessage msg(PPushMsg pushMsg) throws IOException {
-		super.basePath = new StringBuffer("/v1/notification/inside/msg?from=");
-		super.basePath.append(JettyServer.getServerName());
-		super.body = pushMsg.toByteArray();
+	public PMessage msg(PStringList stringList, String uuid, String type, String task) throws IOException {
+		super.basePath = new StringBuffer("/v2/notification/inside/system?from=");
+		super.basePath.append(JettyServer.getServerName()).append("&uuid=").append(uuid).append("&type=").append(type)
+				.append("&task=").append(task);
+		super.body = stringList.toByteArray();
 		super.contentType = "form-data";
 		return super.postBodyMethod();
 	}
