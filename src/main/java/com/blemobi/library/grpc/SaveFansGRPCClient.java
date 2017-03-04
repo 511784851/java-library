eb081ea4286a7e4de4bf3de76ebbd71b4edb5ed5
HUNTER.POON <hunter.pan@foxmail.com>
2017-03-04 16:46:59 +0800

    UPD:save fans api

--- src/main/java/com/blemobi/library/grpc/SaveFansGRPCClient.java
+++ src/main/java/com/blemobi/library/grpc/SaveFansGRPCClient.java
@@ -44,13 +44,14 @@ public class SaveFansGRPCClient extends BaseGRPCClient {
     protected Boolean doProcess(Object... obj) {
         grpcDataPublishingBlockingStub stub = grpcDataPublishingGrpc.newBlockingStub(channel);
         DataPublishingProtos.PFansFilterParam.Builder filterBuilder = DataPublishingProtos.PFansFilterParam.newBuilder();
-        filterBuilder.setGender((Integer) obj[0]);
+        filterBuilder.setGender((Integer) obj[1]);
         // builder.setNegate(value);
-        filterBuilder.addAllRegion((List<String>) obj[1]);
+        filterBuilder.addAllRegion((List<String>) obj[2]);
         // builder.addSkipUuid(value);
-        filterBuilder.setUuid((String) obj[2]);
+        filterBuilder.setUuid((String) obj[3]);
         DatapublishingInside.PFansSaveParam.Builder saveParam = DatapublishingInside.PFansSaveParam.newBuilder();
         saveParam.setFilter(filterBuilder.build());
+        saveParam.setKey(obj[0].toString());
         DatapublishingInside.PFansSaveParam request = saveParam.build();
         stub.saveFans(request);
         return true;
